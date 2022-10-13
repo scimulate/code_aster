33a422997b1037357e264e849d639f89c467fd2e
Samuel Jacobs <samjacobs@pop-os.localdomain>
2022-10-12 22:56:54 -0400

    Making progress on 14.6p. Process works for docker_std.py.

--- Incomplete/v146p/docker_mpi.py
+++ Incomplete/v146p/docker_mpi.py
@@ -1,1 +1,46 @@
-calibre9_mpi.py
+# encoding: utf-8
+
+"""
+Fichier de configuration WAF pour version parallèle sur Ubuntu 13.6 :
+- Compilateur : GNU
+- MPI         : système (OpenMPI, Ubuntu 13.6)
+- BLAS        : OpenBLAS
+- Scalapack   : système (Ubuntu 13.6)
+- PETSc       : 
+"""
+
+import docker_std
+
+def configure(self):
+    opts = self.options
+    docker_std.configure(self)
+
+    self.env.prepend_value('LIBPATH', [
+        '/usr/lib/petsc/lib',
+        '/opt/parmetis-4.0.3/lib',
+        '/opt/mumps-5.1.2_mob/lib',])
+
+    self.env.prepend_value('INCLUDES', [
+        '/opt/petsc-3.9.4/linux-metis-mumps/include',
+        '/opt/petsc-3.9.4/include',
+        '/usr/include/superlu',
+        '/opt/parmetis-4.0.3/include',
+        '/opt/mumps-5.1.2_mob/include',])
+
+    self.env.append_value('LIB', ('X11',))
+
+    opts.parallel = True
+
+    opts.enable_mumps  = True
+    opts.mumps_version = '5.1.2'
+    opts.mumps_libs = 'dmumps zmumps smumps cmumps mumps_common pord metis scalapack openblas esmumps scotch scotcherr'
+#    opts.embed_mumps = True
+
+    opts.enable_petsc = True
+    opts.petsc_libs='petsc HYPRE ml'
+#    opts.petsc_libs='petsc'
+#    opts.embed_petsc = True
+
+#    opts.enable_parmetis  = True
+    self.env.append_value('LIB_METIS', ('parmetis'))
+    self.env.append_value('LIB_SCOTCH', ('ptscotch','ptscotcherr','ptscotcherrexit','ptesmumps'))
--- Incomplete/v146p/docker_std.py
+++ Incomplete/v146p/docker_std.py
@@ -1,1 +1,62 @@
-calibre9_std.py
+# encoding: utf-8
+
+"""
+Fichier de configuration WAF pour version séquentielle sur Ubuntu 13.6 :
+- Compilateur : GNU
+- BLAS        : OpenBLAS
+"""
+import os
+
+def configure(self):
+    opts = self.options
+
+    # mfront path
+#    self.env.TFELHOME = '/opt/tfel-3.2.0'
+
+    self.env.append_value('LIBPATH', ['/opt/aster/public/hdf5-1.10.3/lib',
+                                      '/opt/aster/public/med-4.0.0/lib',
+                                      '/opt/aster/public/metis-5.1.0/lib',
+                                      '/opt/aster/public/scotch-6.0.4/lib',
+                                      '/usr/lib/x86_64-linux-gnu'])
+
+    self.env.append_value('INCLUDES', [
+        '/opt/aster/public/hdf5-1.10.3/include',
+        '/opt/aster/public/med-4.0.0/include',
+        '/opt/aster/public/metis-5.1.0/include',
+        '/opt/aster/public/scotch-6.0.4/include',
+        '/usr/include'])
+#        '/opt/tfel-3.2.0/include',
+
+    opts.maths_libs = 'openblas superlu'
+#    opts.embed_math = True
+
+    opts.enable_hdf5 = True
+    opts.hdf5_libs  = 'hdf5 z'
+#    opts.embed_hdf5 = True
+
+    opts.enable_med = True
+    opts.med_libs  = 'med stdc++'
+#    opts.embed_med  = True
+
+    opts.enable_mfront = False
+
+    opts.enable_scotch = True
+#    opts.embed_scotch  = True
+
+    opts.enable_homard = True
+#    opts.embed_aster    = True
+#    opts.embed_fermetur = True
+
+    # add paths for external programs
+#    os.environ['METISDIR'] = '/opt/aster/public/metis-5.1.0'
+#    os.environ['GMSH_BIN_DIR'] = '/opt/aster/public/gmsh-3.0.6-Linux/bin'
+    os.environ['HOMARD_ASTER_ROOT_DIR'] = '/opt/aster/public/homard-11.12'
+
+    opts.with_prog_metis = True
+#    opts.with_prog_gmsh = True
+    # salome: only required by few testcases
+    # europlexus: not available on all platforms
+#    opts.with_prog_miss3d = True
+    opts.with_prog_homard = True
+#    opts.with_prog_ecrevisse = True
+    opts.with_prog_xmgrace = True
