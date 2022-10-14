eef0e8e197ec206515562aa8e19cfa8eec28c479
Samuel Jacobs <samjacobs@pop-os.localdomain>
2022-10-13 23:18:50 -0400

    Making progress on v14.6p.

--- Incomplete/v146p/docker_mpi.py
+++ Incomplete/v146p/docker_mpi.py
@@ -1,46 +1,57 @@
-# encoding: utf-8
+# coding=utf-8
+# --------------------------------------------------------------------
+# Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
+# This file is part of code_aster.
+#
+# code_aster is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# code_aster is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------
 
 """
-Fichier de configuration WAF pour version parallèle sur Ubuntu 13.6 :
-- Compilateur : GNU
-- MPI         : système (OpenMPI, Ubuntu 13.6)
-- BLAS        : OpenBLAS
-- Scalapack   : système (Ubuntu 13.6)
-- PETSc       : 
+Configuration for Scibian 9  MPI
+
+. $HOME/dev/codeaster/devtools/etc/env_unstable_mpi.sh
+
+waf_mpi configure --use-config=scibian9_mpi --prefix=../install/mpi
+waf_mpi install -p
 """
 
 import docker_std
+#YAMMROOT = scibian9_std.YAMMROOT
 
 def configure(self):
     opts = self.options
-    docker_std.configure(self)
-
-    self.env.prepend_value('LIBPATH', [
-        '/usr/lib/petsc/lib',
-        '/opt/parmetis-4.0.3/lib',
-        '/opt/mumps-5.1.2_mob/lib',])
-
-    self.env.prepend_value('INCLUDES', [
-        '/opt/petsc-3.9.4/linux-metis-mumps/include',
-        '/opt/petsc-3.9.4/include',
-        '/usr/include/superlu',
-        '/opt/parmetis-4.0.3/include',
-        '/opt/mumps-5.1.2_mob/include',])
-
-    self.env.append_value('LIB', ('X11',))
 
     opts.parallel = True
+    docker_std.configure(self)
+    self.env['ADDMEM'] = 500
 
-    opts.enable_mumps  = True
-    opts.mumps_version = '5.1.2'
-    opts.mumps_libs = 'dmumps zmumps smumps cmumps mumps_common pord metis scalapack openblas esmumps scotch scotcherr'
-#    opts.embed_mumps = True
+    self.env.prepend_value('LIBPATH', [
+        '/usr/lib/petsc',
+        #YAMMROOT + '/prerequisites/Parmetis_aster-403_aster3/lib',
+        #YAMMROOT + '/prerequisites/Scotch_aster-604_aster7/MPI/lib',
+        #YAMMROOT + '/prerequisites/Mumps-512_consortium_aster3/MPI/lib',
+        #YAMMROOT + '/prerequisites/Petsc_mpi-394_aster/lib',
+    ])
+
+    self.env.prepend_value('INCLUDES', [
+        'usr/include/petsc',
+        #YAMMROOT + '/prerequisites/Parmetis_aster-403_aster3/include',
+        #YAMMROOT + '/prerequisites/Scotch_aster-604_aster7/MPI/include',
+        #YAMMROOT + '/prerequisites/Mumps-512_consortium_aster3/MPI/include',
+        #YAMMROOT + '/prerequisites/Petsc_mpi-394_aster/include',
+    ])
 
     opts.enable_petsc = True
-    opts.petsc_libs='petsc HYPRE ml'
-#    opts.petsc_libs='petsc'
-#    opts.embed_petsc = True
-
-#    opts.enable_parmetis  = True
     self.env.append_value('LIB_METIS', ('parmetis'))
-    self.env.append_value('LIB_SCOTCH', ('ptscotch','ptscotcherr','ptscotcherrexit','ptesmumps'))
+    self.env.append_value('LIB_SCOTCH', ('ptscotch','ptscotcherr','ptscotcherrexit'))
--- Incomplete/v146p/docker_std.py
+++ Incomplete/v146p/docker_std.py
@@ -1,62 +1,89 @@
-# encoding: utf-8
+# coding=utf-8
+# --------------------------------------------------------------------
+# Copyright (C) 1991 - 2019 - EDF R&D - www.code-aster.org
+# This file is part of code_aster.
+#
+# code_aster is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# code_aster is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with code_aster.  If not, see <http://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------
 
 """
-Fichier de configuration WAF pour version séquentielle sur Ubuntu 13.6 :
-- Compilateur : GNU
-- BLAS        : OpenBLAS
+Configuration for Scibian 9
+
+. $HOME/dev/codeaster/devtools/etc/env_unstable.sh
+
+waf configure --use-config=scibian9_std --prefix=../install/std
+waf install -p
 """
+
 import os
+ASTER_ROOT = '/opt/aster'
+YAMMROOT = ''
+
+import official_programs
+
 
 def configure(self):
     opts = self.options
 
-    # mfront path
-#    self.env.TFELHOME = '/opt/tfel-3.2.0'
+    #official_programs.configure(self)
+    #official_programs.check_prerequisites_package(self, YAMMROOT, '20190513')
 
-    self.env.append_value('LIBPATH', ['/opt/aster/public/hdf5-1.10.3/lib',
-                                      '/opt/aster/public/med-4.0.0/lib',
-                                      '/opt/aster/public/metis-5.1.0/lib',
-                                      '/opt/aster/public/scotch-6.0.4/lib',
-                                      '/usr/lib/x86_64-linux-gnu'])
+    self.env.append_value('CXXFLAGS', ['-D_GLIBCXX_USE_CXX11_ABI=0'])
+    self.env['ADDMEM'] = 350
+
+    TFELHOME = '/opt/aster/public/tfel-3.2.1'
+    TFELVERS = '3.2.1'
+    self.env.TFELHOME = TFELHOME
+    self.env.TFELVERS = TFELVERS
+
+    self.env.append_value('LIBPATH', [
+        '/opt/aster/public/hdf5-1.10.3/lib',
+        '/opt/aster/public/med-4.0.0/lib',
+        '/opt/aster/public/metis-5.1.0/lib',
+        '/opt/aster/public/mumps-5.1.2/lib',
+        '/opt/aster/public/scotch-6.0.4/lib',
+        '/opt/aster/public/tfel-3.2.1/lib',
+    ])
 
     self.env.append_value('INCLUDES', [
         '/opt/aster/public/hdf5-1.10.3/include',
         '/opt/aster/public/med-4.0.0/include',
         '/opt/aster/public/metis-5.1.0/include',
+        '/opt/aster/public/mumps-5.1.2/include',
         '/opt/aster/public/scotch-6.0.4/include',
-        '/usr/include'])
-#        '/opt/tfel-3.2.0/include',
+        '/opt/aster/public/tfel-3.2.1/include',
+    ])
 
+    self.env.append_value('LIB', ('pthread', 'util'))
+    self.env.append_value('LIB_SCOTCH', ('scotcherrexit'))
+    # to fail if not found
+    
     opts.maths_libs = 'openblas superlu'
-#    opts.embed_math = True
-
+    
     opts.enable_hdf5 = True
-    opts.hdf5_libs  = 'hdf5 z'
-#    opts.embed_hdf5 = True
-
-    opts.enable_med = True
-    opts.med_libs  = 'med stdc++'
-#    opts.embed_med  = True
-
-    opts.enable_mfront = False
-
-    opts.enable_scotch = True
-#    opts.embed_scotch  = True
-
     opts.enable_homard = True
-#    opts.embed_aster    = True
-#    opts.embed_fermetur = True
+    opts.enable_med = True
+    opts.enable_metis = False
+    opts.enable_mfront = False    
+    opts.enable_mumps = False
+    opts.enable_scotch = True
 
-    # add paths for external programs
-#    os.environ['METISDIR'] = '/opt/aster/public/metis-5.1.0'
-#    os.environ['GMSH_BIN_DIR'] = '/opt/aster/public/gmsh-3.0.6-Linux/bin'
     os.environ['HOMARD_ASTER_ROOT_DIR'] = '/opt/aster/public/homard-11.12'
 
+    opts.with_prog_gmsh = False
     opts.with_prog_metis = True
-#    opts.with_prog_gmsh = True
-    # salome: only required by few testcases
-    # europlexus: not available on all platforms
-#    opts.with_prog_miss3d = True
+    opts.with_prog_miss3d = False
     opts.with_prog_homard = True
-#    opts.with_prog_ecrevisse = True
+    opts.with_prog_ecrevisse = False
     opts.with_prog_xmgrace = True
